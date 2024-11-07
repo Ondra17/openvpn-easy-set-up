df0c172c8c02a7c41563dac3b57a6b66c2dbc2a1
Ondra17 <145202025+Ondra17@users.noreply.github.com>
2024-11-07 10:18:16 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,25 @@
+import subprocess
+
+def installation():
+    subprocess.run(["dnf", "update", "-y"])
+    subprocess.run(["dnf", "install", "-y", "epel-release"])
+    subprocess.run(["dnf", "install", "-y", "openvpn", "easy-rsa"])
+
+    result = subprocess.run(["openvpn", "--version"], capture_output=True, text=True)
+    if result.returncode == 0:
+        print("################################################")
+        print("#                                              #")
+        print("# OpenVPN and Easy-RSA installed successfully! #")
+        print("#                                              #")
+        print("################################################")
+        print(result.stdout)
+    else:
+        print("OpenVPN installation failed.")
+        print(result.stderr)
+
+
+
+
+installation()
+
 
