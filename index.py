e5286f8bd64373653cf5ca82d3308f0f89115214
Ondra17 <145202025+Ondra17@users.noreply.github.com>
2024-11-13 17:05:06 +0100

    Update index.py

--- index.py
+++ index.py
@@ -24,13 +24,27 @@ def installation():
 def dir_struc():
     subprocess.run(["mkdir", "-p", "/etc/openvpn/easy-rsa/keys"])
 
+    #for EasyRSA repository
+    subprocess.run(["mkdir", "-p", "/opt/easy-rsa"])
+
 def rsa_set_up():
 
+    rep_easy_rsa = glob.glob("/opt/easy-rsa/*")
     all_files = glob.glob("/usr/share/easy-rsa/3/*")
-
-
     subprocess.run(["cp", "-ai"] + all_files + ["/etc/openvpn/easy-rsa"])
-    
+
+
+    if rep_easy_rsa != None:
+        subprocess.run(["rm", "-rf", "/opt/easy-rsa"])
+        subprocess.run(["git", "clone", "https://github.com/OpenVPN/easy-rsa.git", "/opt/easy-rsa/"])
+    else:
+        subprocess.run(["git", "clone", "https://github.com/OpenVPN/easy-rsa.git", "/opt/easy-rsa/"])
+
+    subprocess.run(["cp", "/opt/easy-rsa/easyrsa3/vars.example", "/etc/openvpn/easy-rsa/vars.example"])
+    subprocess.run(["mv", "/etc/openvpn/easy-rsa/vars.example", "/etc/openvpn/easy-rsa/vars"])
+
+
+
 
 
 installation()
