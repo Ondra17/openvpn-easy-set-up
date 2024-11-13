e660c4ca29d5a7117bf838a7d931976fe1c2c159
Ondra17 <145202025+Ondra17@users.noreply.github.com>
2024-11-13 15:34:02 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
-
+import glob
 import subprocess
 
+#Do you want to update DNF repository
+
 def installation():
-    subprocess.run(["dnf", "update", "-y"])
+    #subprocess.run(["dnf", "update", "-y"])
     subprocess.run(["dnf", "install", "-y", "epel-release"])
     subprocess.run(["dnf", "install", "-y", "openvpn", "easy-rsa"])
 
@@ -13,18 +15,26 @@ def installation():
         print("# OpenVPN and Easy-RSA installed successfully! #")
         print("#                                              #")
         print("################################################")
-        print(result.stdout)
+        #print(result.stdout)
     else:
         print("OpenVPN installation failed.")
-        print(result.stderr)
+        #print(result.stderr)
 
 
 def dir_struc():
     subprocess.run(["mkdir", "-p", "/etc/openvpn/easy-rsa/keys"])
 
 def rsa_set_up():
-    subprocess.run(["cp", "-ai", "/usr/share/easy-rsa/3/*", "/etc/openvpn/easy-rsa"])
+
+    all_files = glob.glob("/usr/share/easy-rsa/3/*")
+
+
+    subprocess.run(["cp", "-ai"] + all_files + ["/etc/openvpn/easy-rsa"])
+    
+
 
 installation()
 dir_struc()
 rsa_set_up()
+
+
